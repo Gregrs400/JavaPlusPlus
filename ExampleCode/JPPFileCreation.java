e498dcffeafdc1ed1b7c4acee803030dbc68a589
gregs <gregsalisbury25@gmail.com>
2024-12-08 18:17:50 -0500

    added JPP file creation

--- ExampleCode/JPPFileCreation.java
+++ ExampleCode/JPPFileCreation.java
@@ -1,2 +1,37 @@
-public class JPPFileCreation {
+import java.io.File;
+import java.io.IOException;
+
+public class JPPFileCreation
+{
+
+    public static void main(String[] args)
+    {
+        try
+        {
+
+            File file = new File("JPPExample.jpp");
+
+            if (file.createNewFile())
+            {
+
+                System.out.println("file created");
+
+            }
+            else
+            {
+
+                System.out.println("file already created");
+
+            }
+
+        }
+        catch (IOException e)
+        {
+
+            System.out.println("IOException occurred");
+            e.printStackTrace();
+
+        }
+    }
+
 }
