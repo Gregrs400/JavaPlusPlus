699a78725dc64fa4a60069a2d7f857b86cfa3d2c
gregs <gregsalisbury25@gmail.com>
2024-12-08 20:40:46 -0500

    added refactoring area

--- src/IDE.java
+++ src/IDE.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class IDE
 {
@@ -7,8 +10,16 @@ public class IDE
         JFrame mainFrame = new JFrame("Java Plus Plus IDE");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(null);
+
+        JTextArea refactoringArea = new JTextArea();
+        refactoringArea.setBounds(100, 100, 1200, 500);
+        refactoringArea.setVisible(true);
+
+        mainFrame.add(refactoringArea);
+
         mainFrame.setSize(1400, 1000);
         mainFrame.setLocation(100, 25);
+
         mainFrame.setVisible(true);
 
     }
