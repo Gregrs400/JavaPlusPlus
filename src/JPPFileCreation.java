564ea7da350ea48312e208b9f68635491766d7b5
gregs <gregsalisbury25@gmail.com>
2024-12-08 19:19:32 -0500

    added createOrAccessFile()

--- src/JPPFileCreation.java
+++ src/JPPFileCreation.java
@@ -6,32 +6,33 @@ public class JPPFileCreation
 
     public static void main(String[] args)
     {
-        try
-        {
 
-            File file = new File("JPPExample.jpp");
+        createOrAccessFile("JPPExample.jpp");
 
-            if (file.createNewFile())
-            {
+    }
+    public static void createOrAccessFile(String filePathAndName) {
+
+        try {
+
+            File file = new File(filePathAndName);
+
+            if (file.createNewFile()) {
 
                 System.out.println("file created");
 
-            }
-            else
-            {
+            } else {
 
                 System.out.println("file already created");
 
             }
 
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
 
             System.out.println("IOException occurred");
             e.printStackTrace();
 
         }
+
     }
 
 }
