23bc43b67b574e6854e6057002eef662983192c5
gregs <gregsalisbury25@gmail.com>
2024-12-08 20:12:04 -0500

    set up main window

--- src/IDE.java
+++ src/IDE.java
@@ -3,4 +3,14 @@ import javax.swing.*;
 public class IDE
 {
 
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Java Plus Plus IDE");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLayout(null);
+        mainFrame.setSize(1400, 1000);
+        mainFrame.setLocation(100, 25);
+        mainFrame.setVisible(true);
+
+    }
+
 }
