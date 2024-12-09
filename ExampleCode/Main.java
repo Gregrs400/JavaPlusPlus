bbc603df3a93848c2b25fd1246c74613879e13e7
gregs <gregsalisbury25@gmail.com>
2024-12-08 23:43:26 -0500

    changed input path to relative

--- ExampleCode/Main.java
+++ ExampleCode/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args){
         JavaPlusPlusTokenizer tokenizer = new JavaPlusPlusTokenizer();
         StringBuilder input = new StringBuilder();
-        input = tokenizer.readFile("C:\\Users\\Ryan\\College\\Penn State\\Fall 2024\\Classes\\Compilers\\interpreter_project\\ExampleCode\\MangoCode.txt");
+        input = tokenizer.readFile("ExampleCode\\MangoCode.txt");
         List<Token> tokens = tokenizer.tokenize(input.toString());
         tokenizer.printReport(tokens);
     }
