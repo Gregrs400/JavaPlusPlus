3279f201983b8f1d48f1e8f1a4cc2cf376577767
gregs <gregsalisbury25@gmail.com>
2024-12-08 21:28:16 -0500

    put output area in JPanel and added scroll bars

--- src/IDE.java
+++ src/IDE.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class IDE
 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         JFrame mainFrame = new JFrame("Java Plus Plus IDE");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(null);
@@ -25,10 +25,14 @@ public class IDE
         mainFrame.add(refactoringAreaPanel);
 
         JTextArea outputArea = new JTextArea();
-        outputArea.setBounds(100, 650, 1200, 250);
-        outputArea.setVisible(true);
+        JScrollPane outputAreaScroll = new JScrollPane(outputArea);
 
-        mainFrame.add(outputArea);
+        JPanel outputAreaPanel = new JPanel();
+        outputAreaPanel.setLayout(new BorderLayout());
+        outputAreaPanel.add(outputAreaScroll, BorderLayout.CENTER);
+        outputAreaPanel.setBounds(100, 650, 1200, 250);
+
+        mainFrame.add(outputAreaPanel);
 
         mainFrame.setVisible(true);
 
