25494e29d6b73a785f6f2bdccd40171524f863e9
gregs <gregsalisbury25@gmail.com>
2024-12-08 21:19:52 -0500

    put refactoring area inside panel and added scroll bars

--- src/IDE.java
+++ src/IDE.java
@@ -10,21 +10,25 @@ public class IDE
         JFrame mainFrame = new JFrame("Java Plus Plus IDE");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(null);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setSize(1400, 1000);
+        mainFrame.setLocation(100, 25);
 
         JTextArea refactoringArea = new JTextArea();
-        refactoringArea.setBounds(100, 100, 1200, 450);
-        refactoringArea.setVisible(true);
+        JScrollPane refactoringAreaScroll = new JScrollPane(refactoringArea);
 
-        mainFrame.add(refactoringArea);
+        JPanel refactoringAreaPanel = new JPanel();
+        refactoringAreaPanel.setLayout(new BorderLayout());
+        refactoringAreaPanel.add(refactoringAreaScroll, BorderLayout.CENTER);
+        refactoringAreaPanel.setBounds(100, 100, 1200, 450);
+
+        mainFrame.add(refactoringAreaPanel);
 
         JTextArea outputArea = new JTextArea();
         outputArea.setBounds(100, 650, 1200, 250);
         outputArea.setVisible(true);
 
         mainFrame.add(outputArea);
-
-        mainFrame.setSize(1400, 1000);
-        mainFrame.setLocation(100, 25);
 
         mainFrame.setVisible(true);
 
