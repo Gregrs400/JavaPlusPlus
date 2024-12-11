d466081d3714beac9c055c4ddbbeb2115b713c64
gregs <gregsalisbury25@gmail.com>
2024-12-11 11:21:39 -0500

    removed example code strings and correct input declaration

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
         JavaPlusPlusTokenizer tokenizer = new JavaPlusPlusTokenizer();
-        StringBuilder input = new StringBuilder();
+        StringBuilder input;
         input = tokenizer.readFile("src\\ExampleCode\\Java++CodeExample1.txt");
         System.out.println(input.toString());
         List<Token> tokens = tokenizer.tokenize(input.toString());
         for (Token token : tokens) {
             System.out.print(token.toString());
         }
-//        String str1 = "if";
-//        String str2 = "if elif else";
-//        String str3 = "for int i = 0";
-//        String str4 = "while x = myNum";
-//        String str5 = "public str myString";
-//        String str6 = "private char grade";
-//        String str7 = "+ - * / % =";
-//        String str8 = "private bool flag";
-//        String str9 = "\"Hello\"";
-//        String str10 = " 9_4a";
-//        String str11 = "numOne++";
-//        String str12 = "+ class Person { }";
+
     }
 }
