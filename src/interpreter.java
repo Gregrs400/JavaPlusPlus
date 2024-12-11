8c50d3aa8a74b1f50b7c9ecadc8f7807e31cb58f
gregs <gregsalisbury25@gmail.com>
2024-12-11 09:40:09 -0500

    removed arrays of keywords

--- src/interpreter.java
+++ src/interpreter.java
@@ -11,20 +11,6 @@ public class interpreter
 
     public void interpret()
     {
-        String[] javaKeywords = {"abstract", "assert", "boolean", "break", "byte",
-            "case", "catch", "char", "class", "const", "continue", "default", "do",
-            "double", "else", "enum", "extends", "final", "finally", "float", "for",
-            "goto", "if", "implements", "import", "instanceof", "int", "interface",
-            "long", "native", "new", "package", "private", "protected", "public",
-            "return", "short", "static", "strictfp", "super", "switch", "synchronized",
-            "this", "throw", "throws", "transient", "try", "void", "volatile", "while"};
-
-        String[] javaPlusPlusKeywords = {"abstract", "assert", "break", "case", "catch",
-            "continue", "default", "do", "elif", "else", "enum", "extends", "final",
-            "finally", "for", "func", "if", "implements", "import", "in", "instanceof",
-            "native", "new", "package", "return", "static", "strictfp", "super", "switch",
-            "synchronized", "throw", "throws", "transient", "try", "var", "void",
-            "volatile", "while"};
 
         HashMap<String, String> keywords = new HashMap<>();
         HashMap<String, String> dataTypes = new HashMap<>();
